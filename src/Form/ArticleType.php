2b928dc1e276918613feadb6b443209c0f463cfa
Delrodie AMOIKON <delrodieamoikon@gmail.com>
2020-05-12 11:26:49 +0000

    MAJ articleType

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -24,7 +24,7 @@ class ArticleType extends AbstractType
             ->add('lien', TextType::class,['attr'=>['class'=>'form-control', 'placeholder'=>"Le lien de la video", 'autocomplete'=>"off"]])
             ->add('auteur', TextType::class,['attr'=>['class'=>'form-control', 'placeholder'=>"L'auteur de la video", 'autocomplete'=>"off"]])
             ->add('tag', TextType::class,['attr'=>['class'=>'form-control tags', 'placeholder'=>'Les mots clés', 'width'=>"100%"]])
-            ->add('publieLe', TextType::class,['attr'=>['class'=>'form-control datepicker']])
+            ->add('publieLe', TextType::class,['attr'=>['class'=>'form-control datepicker', 'autocomplete'=>'off']])
             ->add('rubrique', EntityType::class,[
                 'attr'=>['class'=>'form-control custom-select rubrique-select', 'width'=>"100%"],
                 'class'=> 'App\Entity\Rubrique',
